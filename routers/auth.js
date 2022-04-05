de6b6ca88fddcaf7b67f83591f23bebd6370857f
Derek Schwantner <derek@Dereks-Mac-mini.local>
2022-04-05 00:40:15 -0400

    added users router

---
+++ routers/auth.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+// const Users = require('../models/users-model')
+
+const router = express()
+
+  // Adding users on signup 
+router.route("/").post(async (req, res) => {
+    if (!req.body.email) {
+      return res.status(400).json({
+        error: `Error during ${req.method} at ${req.originalUrl}: email is required.`
+      });
+    }
+    // Check if email of user exists to decide whether to create new user
+    const currentUser = await Users.find({ email: req.body.email }).first();
+    if (currentUser) {
+      // Return user info
+      const userInfo = { currentUser };
+      return res.status(200).json({ userInfo });
+    } else {
+      // Add user to database if email does not exist
+      const newUser = await Users.add(req.body);
+      const userInfo = { newUser };
+      res.status(201).json({ userInfo });
+    }
+  });
+
+
+  module.exports = router;
