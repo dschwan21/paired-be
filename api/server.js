ee1bd7bb234e06471f90a3639fef421604e4a428
Derek Schwantner <derek@Dereks-Mac-mini.local>
2022-05-04 00:21:04 -0400

    added users endpoint to server

--- api/server.js
+++ api/server.js
@@ -13,16 +13,16 @@ const server = express();
 
 // Routers
 const authRouter = require("../routers/auth");
-// const usersRouter = require("../routers/users-router");
+const usersRouter = require("../routers/users-router");
 // const testRouter = require("../routers/token-tester")
 
 // API Endpoints
 server.use("/api/auth", authRouter);
-// server.use("/api/users", usersRouter);
+server.use("/api/users", usersRouter);
 // server.use("/api/test", testRouter);
 
 server.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello Paired!!');
 });
 
 module.exports = server;
